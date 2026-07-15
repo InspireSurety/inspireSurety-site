0c32e97ef8315b720d156819aa5c9dffc5e09d8b
Steve <swright@inspiresurety.com>
2026-07-15 07:11:31 -0700

    fix markdown frontmatter parsing

--- .eleventy.cjs.js
+++ .eleventy.cjs.js
@@ -3,14 +3,19 @@ module.exports = function(eleventyConfig) {
   // Pass through assets
   eleventyConfig.addPassthroughCopy("assets");
 
+  // Set frontmatter options
+  eleventyConfig.setFrontMatterParsingOptions({
+    excerpt: true,
+    excerpt_separator: "<!-- excerpt -->"
+  });
+
   return {
-    templateFormats: ["md", "njk", "html"],
-    markdownTemplateEngine: "liquid",
+    templateFormats: ["md", "njk", "html", "liquid"],
+    markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
     dir: {
       input: ".",
       includes: "_includes",
-      layouts: "_includes",
       data: "_data",
       output: "_site"
     }
