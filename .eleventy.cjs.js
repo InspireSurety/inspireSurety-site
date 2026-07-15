426390f44773250338eabae1eb9bd349c2fef9cd
Steve <swright@inspiresurety.com>
2026-07-15 06:47:07 -0700

    fix eleventy config and layout styling

--- .eleventy.cjs.js
+++ .eleventy.cjs.js
@@ -1,6 +1,18 @@
-module.exports = {
-  dir: {
-    input: ".",
-    output: "_site"
-  }
+module.exports = function(eleventyConfig) {
+
+  // Pass through assets
+  eleventyConfig.addPassthroughCopy("assets");
+
+  return {
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "liquid",
+    htmlTemplateEngine: "njk",
+    dir: {
+      input: ".",
+      includes: "_includes",
+      layouts: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+  };
 };
